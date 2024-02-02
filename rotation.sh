e7414677daa75a2f4f252c75cc8e15f641ae816d
AbesM <128533975+AbesM@users.noreply.github.com>
2024-02-02 15:02:35 +0100

    Add files via upload

---
+++ rotation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sleep 666
+while ((1)); do
+xrandr --output HDMI-A-0 --transform 0.5,0.5,0,-0.5,0.5,0,0,0,1
+sleep 5
+xrandr --output HDMI-A-0 --transform 1,0,0,0,1,0,0,0,1 
+sleep 666
+done
