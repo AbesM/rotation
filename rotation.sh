9d7f6323635c60bb00cf4053803547addfe21b99
AbesM <128533975+AbesM@users.noreply.github.com>
2024-02-02 15:19:42 +0100

    Update rotation.sh

--- rotation.sh
+++ rotation.sh
@@ -3,7 +3,7 @@
 sleep 666
 while ((1)); do
 xrandr --output HDMI-A-0 --transform 0.5,0.5,0,-0.5,0.5,0,0,0,1
-sleep 5
+sleep 15
 xrandr --output HDMI-A-0 --transform 1,0,0,0,1,0,0,0,1 
 sleep 666
 done
